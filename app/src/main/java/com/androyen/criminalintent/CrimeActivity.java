e79ec62032380a71e26a97ee98141184061a774f
Androyen <androyen@aol.com>
2014-11-01 20:43:37 -0500

    CrimeActivity updated to inherit from FragmentActvity

--- app/src/main/java/com/androyen/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/androyen/criminalintent/CrimeActivity.java
@@ -1,12 +1,14 @@
 package com.androyen.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class CrimeActivity extends ActionBarActivity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +17,7 @@ public class CrimeActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.crime, menu);
-        return true;
-    }
+   
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
