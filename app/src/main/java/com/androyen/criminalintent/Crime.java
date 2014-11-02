bbe98af82fced3600701ef0e67ce2ebb5f243524
Androyen <androyen@aol.com>
2014-11-01 22:40:51 -0500

    Add date and isSolved to Crime.java

--- app/src/main/java/com/androyen/criminalintent/Crime.java
+++ app/src/main/java/com/androyen/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.androyen.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -11,10 +12,15 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         //Generate unique identifier for Crime
         mId = UUID.randomUUID();
+
+        //Create new Date  Sets date to the current time created
+        mDate = new Date();
     }
 
     //Getter and setter
@@ -29,5 +35,22 @@ public class Crime {
     public void setTitle(String title) {
         mTitle = title;
     }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 
 }
