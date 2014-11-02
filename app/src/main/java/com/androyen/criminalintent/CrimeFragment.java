5d4a8c2f21cf3064082748852eb9f660d3e57b5d
Androyen <androyen@aol.com>
2014-11-01 22:04:01 -0500

    Implement EditText addTextChangeListener

--- app/src/main/java/com/androyen/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/androyen/criminalintent/CrimeFragment.java
@@ -2,6 +2,12 @@ package com.androyen.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by rnguyen on 11/1/14.
@@ -11,6 +17,7 @@ import android.support.v4.app.Fragment;
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,35 @@ public class CrimeFragment extends Fragment {
 
         //Initialize crime during creation
         mCrime = new Crime();
+    }
+
+    //Inflate fragment.xml view
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence c, int start, int before, int count) {
+
+                //Listener for EditText  When typed and changed, set title
+                mCrime.setTitle(c.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
     }
 
 }
