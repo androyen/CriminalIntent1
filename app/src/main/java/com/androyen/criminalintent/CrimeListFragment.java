7026a243208893895e5631bf9cd407ef3e46aa12
Androyen <androyen@aol.com>
2014-11-02 00:58:43 -0500

    Custom ListView in ListFragment. Create list_item_crime.xml

--- app/src/main/java/com/androyen/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/androyen/criminalintent/CrimeListFragment.java
@@ -2,7 +2,10 @@ package com.androyen.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -12,6 +15,7 @@ import java.util.ArrayList;
 public class CrimeListFragment extends ListFragment {
 
     private ArrayList<Crime> mCrimes;
+    private static final String TAG = CrimeListFragment.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,14 @@ public class CrimeListFragment extends ListFragment {
         ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);
         setListAdapter(adapter);
 
+    }
+
+    //Implements onListItemClick to respond to selection of ListView
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+
+        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Log.d(TAG, c.getTitle() + " was clicked");
     }
 
 }
